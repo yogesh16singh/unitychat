a3d975284888c3cb6cc079323b4f42a1c5057ff9
Yogesh Singh <110375635+yogesh16singh@users.noreply.github.com>
2024-03-02 04:54:04 +0000

    feat : Added Theme

--- src/app/(main)/(routes)/page.tsx
+++ src/app/(main)/(routes)/page.tsx
@@ -1,10 +1,14 @@
 import { UserButton } from "@clerk/nextjs";
+import { ModeToggle
+ } from "@/components/mode-toggle";
 
 const Home=()=>{
   return <div>
      <UserButton
         afterSignOutUrl="/"
       />
+      <ModeToggle />
+
   </div>
 }
 export default Home;
