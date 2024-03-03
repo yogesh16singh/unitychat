f1b3a242ad122b405abe100e1492110fe7091b6f
Yogesh Singh <110375635+yogesh16singh@users.noreply.github.com>
2024-03-03 14:32:06 +0000

    feat : initial modal

--- src/app/(setup)/page.tsx
+++ src/app/(setup)/page.tsx
@@ -2,7 +2,7 @@ import { redirect } from "next/navigation";
 
 import { db } from "@/lib/db";
 import { initialProfile } from "@/lib/initial-profile";
-
+import { InitialModal } from "@/components/modals/initial-modal";
 const SetupPage = async () => {
   console.log('setup page')
   const profile = await initialProfile();
@@ -21,7 +21,7 @@ const SetupPage = async () => {
     return redirect(`/servers/${server.id}`);
   }
 
-  return <div>Create a Server</div>;
+  return <InitialModal />;
 }
 
 export default SetupPage;
