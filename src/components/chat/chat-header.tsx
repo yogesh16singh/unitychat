b9a448c2aea1c940e889bf167e64eda63e53be2b
Yogesh Singh <110375635+yogesh16singh@users.noreply.github.com>
2024-03-12 06:50:32 +0000

    feat : conversations

--- src/components/chat/chat-header.tsx
+++ src/components/chat/chat-header.tsx
@@ -1,6 +1,7 @@
 import { Hash } from "lucide-react";
 
 import { MobileToggle } from "@/components/mobile-toggle";
+import { UserAvatar } from "@/components/user-avatar";
 
 interface ChatHeaderProps {
   serverId: string;
@@ -13,7 +14,7 @@ export const ChatHeader = ({
   serverId,
   name,
   type,
-  imageUrl
+  imageUrl,
 }: ChatHeaderProps) => {
   return (
     <div className="text-md font-semibold px-3 flex items-center h-12 border-neutral-200 dark:border-neutral-800 border-b-2">
@@ -21,9 +22,10 @@ export const ChatHeader = ({
       {type === "channel" && (
         <Hash className="w-5 h-5 text-zinc-500 dark:text-zinc-400 mr-2" />
       )}
-      <p className="font-semibold text-md text-black dark:text-white">
-        {name}
-      </p>
+      {type === "conversation" && (
+        <UserAvatar src={imageUrl} className="h-8 w-8 md:h-8 md:w-8 mr-2" />
+      )}
+      <p className="font-semibold text-md text-black dark:text-white">{name}</p>
     </div>
-  )
-}
+  );
+};
